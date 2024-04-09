0fb3998d0ab79250de7175a8fbbaae888ec8bce5
brian <bhau@sfu.ca>
2024-04-08 21:49:56 -0700

    -Commenting locally added

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -9,13 +9,42 @@ import { UserService } from 'src/app/services/user.service';
 export class MessagesComponent implements OnInit {
   title: string = '';
   text: string = '';
-
-  constructor() { }
+  firstName: string = '';
+  lastName: string  = '';
+  commentInput: string = ''; // Initialize comment input variable
+  comments = [{
+    firstName: 'Missing',
+    lastName: 'Missing',
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id tellus et magna ultricies congue gravida eu lacus. Praesent in nunc urna.'
+  }]
+  loggedInUserId: string = sessionStorage.getItem('loggedInUserId')!;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.extractUrlParameters();
+    this.getUserInfo();
   }
-
+  getUserInfo(){
+    this.userService.getUser(this.loggedInUserId)?.subscribe(
+      res => {
+        this.firstName = res.data.firstName;
+        this.lastName  = res.data.lastName;
+      },
+      error => {
+        console.error('Failed to fetch user:', error);
+      }
+  );
+  }
+  submitComment() {
+    if(this.commentInput == '') return;
+    this.comments.push({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      comment: this.commentInput 
+    });
+    this.commentInput = '';
+  }
+  
   extractUrlParameters() {
     const urlParams = new URLSearchParams(window.location.search);
 
